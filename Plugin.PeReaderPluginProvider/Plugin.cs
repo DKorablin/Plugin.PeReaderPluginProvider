1ce2aa02c21704e380c0091a2a01d80d47bc139b
DKorablin2 <danila.korablin@unity3d.com>
2025-11-04 16:03:41 +0300

    Fixed error when StackOverflowPrevention triggers for referenced assemblies somehow...

--- Plugin.PeReaderPluginProvider/Plugin.cs
+++ Plugin.PeReaderPluginProvider/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -13,7 +14,12 @@ namespace Plugin.PeReaderPluginProvider
 	/// <summary>Plugins loader from file system but it's using separate sandbox to find appropriate assemblies to load</summary>
 	public class Plugin : IPluginProvider
 	{
-		private readonly HashSet<String> _recursiveAssemblyNameCheck = new HashSet<String>();
+		// This is used to prevent stack overflow when trying to resolve assembly recursively
+		private readonly ConcurrentDictionary<String, Int32> _recursiveAssemblyNameCheck = new ConcurrentDictionary<String, Int32>();
+
+		// The list of files to ignore during loading to prevent multiple attempts to load invalid assemblies
+		private readonly HashSet<String> _badFilesList = new HashSet<String>();
+
 		private TraceSource _trace;
 
 		private TraceSource Trace { get => this._trace ?? (this._trace = Plugin.CreateTraceSource<Plugin>()); }
@@ -86,13 +92,19 @@ namespace Plugin.PeReaderPluginProvider
 			if(String.IsNullOrEmpty(assemblyName))
 				throw new ArgumentNullException(nameof(assemblyName), "Assembly name is required to resolve it");
 
-			Boolean isSuccess = this._recursiveAssemblyNameCheck.Add(assemblyName);
-			if(isSuccess)
-			{//This check is used when PluginProvider tries to resolve assembly while resolving assembly with the same name
+			Int32 requestCount = _recursiveAssemblyNameCheck.AddOrUpdate(assemblyName, 1, (key, value) => value + 1);
+			if(requestCount > 2)
+			{//This check is used when PluginProvider tries to resolve assembly while resolving assembly with the same name. (Fixed issues while loading: System.ServiceModel.Primitives.dll and CoreWCF.Primitives.dll)
+				this.Trace.TraceEvent(TraceEventType.Information, 5, "StackOverflowPrevention: Assembly {0} already requested to load", assemblyName);
+				return FallBackToParent();
+			}
+
+			try
+			{
 				AssemblyName targetName = new AssemblyName(assemblyName);
 				foreach(String pluginPath in this.Args.PluginPath.Where(p => Directory.Exists(p)))
 					foreach(String file in Directory.EnumerateFiles(pluginPath, "*.*", SearchOption.AllDirectories)
-						.Where(f => FilePluginArgs.CheckFileExtension(f)))
+						.Where(f => FilePluginArgs.CheckFileExtension(f) && !_badFilesList.Contains(f)))
 						try
 						{
 							AssemblyName name = AssemblyName.GetAssemblyName(file);
@@ -101,23 +113,33 @@ namespace Plugin.PeReaderPluginProvider
 							//return assembly;//TODO: Reference DLL from operating system are not working
 						} catch(BadImageFormatException)
 						{
+							_badFilesList.Add(file);
 							// Ignoring BadImageFormatException
 						} catch(FileLoadException)
 						{
+							_badFilesList.Add(file);
 							// Ignoring FileLoadException
 						} catch(Exception exc)
 						{
+							_badFilesList.Add(file);
+
 							exc.Data.Add("Library", file);
 							this.Trace.TraceData(TraceEventType.Error, 1, exc);
 						}
-				this._recursiveAssemblyNameCheck.Remove(assemblyName);
-				this.Trace.TraceEvent(TraceEventType.Warning, 5, "Assembly {0} can't be resolved in path {1} by provider {2}", assemblyName, String.Join(",", this.Args.PluginPath), this.GetType());
-			} else
-				this.Trace.TraceEvent(TraceEventType.Information, 5, "StackOverflowPrevention: Assembly {0} already requested to load", assemblyName);
 
-			this.Trace.TraceEvent(TraceEventType.Warning, 5, "The provider {2} is unable to locate the assembly {0} in the path {1}", assemblyName, String.Join(",", this.Args.PluginPath), this.GetType());
-			IPluginProvider parentProvider = ((IPluginProvider)this).ParentProvider;
-			return parentProvider?.ResolveAssembly(assemblyName);
+				this.Trace.TraceEvent(TraceEventType.Warning, 5, "Assembly {0} can't be resolved in path {1} by provider {2} (attempt {3})", assemblyName, String.Join(",", this.Args.PluginPath), this.GetType(), requestCount);
+				return FallBackToParent();
+			} finally
+			{
+				_recursiveAssemblyNameCheck.AddOrUpdate(assemblyName, 0, (key, value) => value - 1);
+			}
+
+			Assembly FallBackToParent()
+			{
+				this.Trace.TraceEvent(TraceEventType.Warning, 5, "The provider {2} is unable to locate the assembly {0} in the path {1}", assemblyName, String.Join(",", this.Args.PluginPath), this.GetType());
+				IPluginProvider parentProvider = ((IPluginProvider)this).ParentProvider;
+				return parentProvider?.ResolveAssembly(assemblyName);
+			}
 		}
 
 		/// <summary>New file for check is available</summary>
